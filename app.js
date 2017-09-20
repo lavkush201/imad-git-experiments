4aaf4624915f07bfea93bad294b4ec7883a21139
Lokesh singh <lokeshsingh.585@gmail.com>
2017-09-21 00:39:24 +0530

    Halfway done with new feature, tested so far, all good.

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-  function somehing() {
+  function aBigNewFeature(){
+  	console.log('Writing a lot of code.');
+	console.log('Halfway done with thsi new features now.');
+   };
+  function something() {
    console.log('I am something');
    }
   console.log("hello world");
   something();
+  aBigNewFeature();
+
 
