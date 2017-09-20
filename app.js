1ba2cea78c35211afb2e67c039d9396b6749bd10
Lokesh singh <lokeshsingh.585@gmail.com>
2017-09-21 01:03:36 +0530

    New featrure completed.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
   function aBigNewFeature(){
   	console.log('Writing a lot of code.');
 	console.log('Halfway done with thsi new features now.');
+        console.log('Completed with the new feature');
    };
   function something() {
    console.log('I am something');
